1bab08c02371d16af4e3b239373bdc039438fdbc
Eliel Roger <elielrogernic@gmail.com>
2020-07-24 21:16:04 -0300

    style(App.tsx): Define background color and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 export default function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <GlobalStyles/>
     </div>
   )
 };
